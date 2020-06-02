6749af5fedcd1eb99fa95e2efd317ec7c1756b86
苏晓慧 <suxh@fadada.com>
2020-06-02 20:40:36 +0800

    测试提交

--- chapter1/src/main/java/nia/chapter1/ConnectHandler.java
+++ chapter1/src/main/java/nia/chapter1/ConnectHandler.java
@@ -11,6 +11,8 @@ import io.netty.channel.ChannelInboundHandlerAdapter;
 public class ConnectHandler extends ChannelInboundHandlerAdapter {
     @Override
     //当一个新的连接已经被建立时，channelActive(ChannelHandlerContext)将会被调用
+
+
     public void channelActive(ChannelHandlerContext ctx)
             throws Exception {
         System.out.println(
